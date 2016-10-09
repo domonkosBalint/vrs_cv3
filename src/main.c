ab55cb63e28acc7bfb7a3746c75eb47da90056ff
domonkosBalint <4balint@gmail.com>
2016-10-09 23:20:58 +0200

    Uloha_2

--- src/main.c
+++ src/main.c
@@ -70,23 +70,34 @@ int main(void)
   /* TODO - Add your application code here */
 
 
-  	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
 
-    GPIO_InitTypeDef gpioInitStruc;
+  GPIO_InitTypeDef gpioInitStruc;
 
-    gpioInitStruc.GPIO_Mode = GPIO_Mode_OUT;
-    gpioInitStruc.GPIO_OType = GPIO_OType_PP;
-    gpioInitStruc.GPIO_Pin = GPIO_Pin_5;
-    gpioInitStruc.GPIO_Speed = GPIO_Speed_40MHz;
+  gpioInitStruc.GPIO_Mode = GPIO_Mode_OUT;
+  gpioInitStruc.GPIO_OType = GPIO_OType_PP;
+  gpioInitStruc.GPIO_Pin = GPIO_Pin_5;
+  gpioInitStruc.GPIO_Speed = GPIO_Speed_400KHz;
 
-    GPIO_Init(GPIOA, &gpioInitStruc);
+  GPIO_Init(GPIOA, &gpioInitStruc);
+
+ // GPIO_SetBits(GPIOA, GPIO_Pin_5);
+
+  gpioInitStruc.GPIO_Mode = GPIO_Mode_IN;
+  gpioInitStruc.GPIO_PuPd = GPIO_PuPd_UP;
+  gpioInitStruc.GPIO_Pin = GPIO_Pin_13;
+  gpioInitStruc.GPIO_Speed = GPIO_Speed_40MHz;
+
+  GPIO_Init(GPIOC, &gpioInitStruc);
+
+  uint8_t buttonState = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13);
+  GPIO_ResetBits(GPIOA, GPIO_Pin_5);
 
   /* Infinite loop */
   while (1)
   {
 	i++;
-
-	GPIO_SetBits(GPIOA, GPIO_Pin_5);
   }
   return 0;
 }
